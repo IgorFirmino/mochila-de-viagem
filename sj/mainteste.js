b12edfbc837d44f28056924bfc0ced01e87f9557
Igor Dos Santos Firmino <105456978+IgorFirmino@users.noreply.github.com>
2023-03-31 12:08:24 -0300

    primeiro commit

---
+++ sj/mainteste.js
@@ -0,0 +1,19 @@
+const itens =  {
+    "nome" : 'igor',
+    "idade": '22',
+    "peso" : 80
+}
+
+const itensw =  {
+    "nome" : 'igor',
+    "idade": '22',
+    "peso" : 80
+}
+
+    itens.id = 10
+
+
+
+
+
+console.log(itens.id)
